9206ddf6e982f34c1ad3af7ee2c45daf75fb2e58
Anirudh <rudysback@gmail.com>
2014-06-20 18:07:50 -0500

    initial commit

---
+++ DAL/PhoneBookContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using PhoneBook.Models;
+
+namespace PhoneBook.DAL
+{
+    public class PhoneBookContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
