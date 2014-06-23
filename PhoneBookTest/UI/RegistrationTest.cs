36af2a6e7d93abe3f2a888038af2645f767e32e4
cameron-r <crowshanbin@gmail.com>
2014-06-23 13:47:52 -0500

    Cameron & Anirudh | Changed directory of UI tests

---
+++ PhoneBookTest/UI/RegistrationTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PhoneBookTest.Feature
+{
+    class RegistrationTests
+    {
+        [Test]
+        public void GivenAUserIsntRegistered_WhenRegisteringThem_TheyEndUpBackOnTheHomepageAndLoggedIn()
+        {
+            var page = Host.Instance.NavigateToInitialPage<HomePage>();
+
+            Assert.That(page.Title, Is.EqualTo("Log in - Phone Book"));
+        }
+    }
+}
