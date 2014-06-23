a110fd3d5575781734b9a1f7171eb2595fd4bbea
Anirudh <rudysback@gmail.com>
2014-06-23 11:37:38 -0500

    Setup UI Test

---
+++ PhoneBookTest/Feature/HomePage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.Seleno.PageObjects;
+
+namespace PhoneBookTest.Feature
+{
+    public class HomePage : Page
+    {
+        public LoginPanel LoginPanel
+        {
+            get { return GetComponent<LoginPanel>(); }
+        }
+    }
+}
